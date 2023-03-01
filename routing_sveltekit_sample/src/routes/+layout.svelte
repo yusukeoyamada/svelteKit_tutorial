b294b727761636c3dfc2d33a02885560319f64bc
yusukeoyamada <fa19940118@gmail.com>
2023-03-01 15:43:41 +0900

    * 「初めてSvelteKitを使い始めた人のための基礎講座」教材対応   * 02: SvelteKitのプロジェクトの作成   * 03: ルーティング

---
+++ routing_sveltekit_sample/src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+  // storeがもつpageを利用して、現在アクセス中のページのURLを取得することができます
+  import { page } from '$app/stores';
+
+  // pageオブジェクトの中身からは、現在アクセスしているページのURLとして
+  // routeのid、urlのpathnameが利用できる
+    // 「$:」を変数につけると、動的に変更される変数の値の変更に影響されるように
+	$: pathname = $page.url.pathname;
+	// $: console.log(pathname);
+</script>
+
+<nav>
+  <!-- <a href="/">home</a>
+  <a href="/about">about</a> -->
+
+  <!-- 各リンクのaタグの中で、if文による条件分岐を利用 -->
+	<a href="/" class:active={pathname === '/'}>home</a>
+	<a href="/about" class:active={pathname === '/about'}>about</a>
+</nav>
+
+<!-- slotタグの部分に、layoutファイルを利用するページファイルの内容が挿入 -->
+<slot />
+
+<style>
+	.active {
+		color: red;
+		font-weight: 900;
+	}
+</style>
---
+++ routing_sveltekit_sample/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<!-- 以下は、共通内容なので、「routes/+layout.svelte」にて、適用 -->
+<!-- <nav>
+  <ul>
+    <li><a href="/about">About</a></li>
+  </ul>
+</nav> -->
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
